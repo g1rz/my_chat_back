09d48e2bffbbccb42048d78d62476e0b562b5a9a
Sergey Peshkov <zloi.g1rz@gmail.com>
2023-11-24 09:22:47 +0800

    Init

---
+++ server/controllers/UserController.js
@@ -0,0 +1,51 @@
+class UserController {
+	async registration(req, res, next) {
+		try {
+
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	async login(req, res, next) {
+		try {
+
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	async logout(req, res, next) {
+		try {
+
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	async activate(req, res, next) {
+		try {
+
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	async refresh(req, res, next) {
+		try {
+
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	async users(req, res, next) {
+		try {
+			res.json(['qwqw', 'qwqwq']);
+		} catch (err) {
+			console.error(err);
+		}
+	}
+}
+
+export default new UserController();
---
+++ server/index.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import router from './router/index.js';
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use('/api', router);
+
+const start = async () => {
+	try {
+		await mongoose.connect(process.env.DB_URL, {})
+		app.listen(PORT, () => {
+			console.log(`Server on port :${PORT} started`);
+		})
+	} catch(e) {
+		console.error(e);
+	}
+}
+
+start();
